e2602dd8a7eecd818fc0a9710872ab6a6dd2bdbe
Abtaheel <abtaheeislam@gmail.com>
2026-08-26 15:22:40 +1000

    slicing revision

--- Week-01/python/slicing.py
+++ Week-01/python/slicing.py
@@ -1,2 +1,49 @@
 nums = [0,1,2,3,4,5,6,7,8,9]
-print(nums[0:3])
+
+# An empty result, without an error
+
+# Question 1
+# print(nums[0:3])
+
+# Question 2
+# print(nums[7:10])
+
+# Question 3
+# print(nums[3:7])
+
+# Question 4
+# print(nums[9:])
+
+# Question 5
+# print(nums[-2:])
+
+# Question 6
+# print(nums[::2])
+
+# Question 7
+# print(nums[1::2])
+
+# Question 8
+# print(nums[::-1])
+
+# Question 9
+# print(nums[::-1][::2])
+
+# Question 10 
+# print(nums[-8:-2])
+
+# Question 11
+# new_nums = nums[:]
+
+# Question 12
+# nums[10:20]
+
+s = "the quick brown fox"
+# Question 13
+# print(s[-3::])
+
+# Question 14
+# print(s[::-1])
+
+# Question 15
+# print(s[::2])
